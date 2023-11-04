82e20978da47afc7a5012e66f789e070aaf368e1
ceeb <test@gmail.com>
2023-11-04 10:26:29 -0700

    init

--- launch-mosaic-vlc.py
+++ launch-mosaic-vlc.py
@@ -2,7 +2,10 @@ import json
 import os
 from PIL import Image, ImageDraw
 from sys import platform
+from datetime import date
 
+### Globals ###
+writeToLogBoolean = False
 mosaicConfFilename = 'mosaic-vlm.conf'
 mosaicImageFilename = 'background.png'
 mosaicExecuteFileWin = 'executeMosaicStreaming.bat'
@@ -12,10 +15,10 @@ osPlatform = 'WIN'
 fullFilePath = ''
 dir_path = os.path.dirname(os.path.realpath(__file__))
 fullFilePath = dir_path + "\\" + mosaicImageFilename
-print(fullFilePath)
-
+LogFilePath = dir_path + "\\log.info"
 mosaicImageFullFilePath = ''
 mosaicConfFullFilePath = ''
+### Globals ###
 
 if platform == "linux" or platform == "linux2":
     osPlatform = 'LIN'
@@ -79,27 +82,49 @@ def createImageFile(list):
 
 def createMosaicConfigFile(list):
     print(mosaicConfFullFilePath)
+	
+    horizontal = list['mosaic']['pixels-horizontal']
+    vertical = list['mosaic']['pixels-vertical']
+    videoCodec = list['mosaic']['videoCodec']
+    audioCodec = list['mosaic']['audioCodec']
+    fps = list['mosaic']['fps']
+    delay = fps = list['mosaic']['delay']
+	
+    if(horizontal == ""):
+	    horizontal = '1280'
+    if(vertical == ""):
+	    vertical = '720'
+		
+    if(videoCodec == ""):
+	    videoCodec = 'mp4v'
+    if(audioCodec == ""):
+	    audioCodec = 'mp4v'
+	
+    if(fps == ""):
+	    fps = '12/1'
+		
     try:
         if os.path.exists(mosaicConfFullFilePath):
             os.remove(mosaicConfFullFilePath)
     except Exception as e:
         print("No file exist")
+        writeToLog("No file exist")
     
     print("Opening file")
+    writeToLog("Opening file")
     
     counter = 1
     order_str = ""
     launch_list = []
     file = open(mosaicConfFullFilePath, "a")
     
-    
     file.write("### VLC (VLM) configuration: Tile mosaic IP Camera  ###\r\n")
+    writeToLog("### VLC (VLM) configuration: Tile mosaic IP Camera  ###\r\n")
     
     for item in list['mosaic']['ipcitem']:
     
         print(item)
         
-     
         ipcCount = "Channel" + str(counter)
         
         file.write("## " + str(counter) + " ##\r\n")
@@ -124,12 +149,15 @@ def createMosaicConfigFile(list):
     
     file.write("## Background ##\r\n")  
     file.write("new background broadcast enabled\r\n")
-    file.write("setup background option image-fps=12/1\r\n")
+    file.write("setup background option image-fps=" + fps + "\r\n")
     if(osPlatform == 'WIN'):
         file.write("setup background input " + "\"" +  dir_path + "\\background.png\"\r\n")
     if(osPlatform == 'LIN'):
         file.write("setup background input " + "\"" +  dir_path + "/background.png\"\r\n")
-    file.write("setup background output #transcode{sfilter=mosaic{width=2560,height=1440,rows=3,cols=2,borderw=2,borderh=2,position=1," + order_str + "},vcodec=mp4v}:display\r\n")
+
+    file.write("setup background output #transcode{sfilter=mosaic{width=" + horizontal + ",height=" + vertical + 
+	",rows=3,cols=2,borderw=2,borderh=2,position=1," + order_str + ",delay=" + delay + "},vcodec=" + videoCodec + ",acodec=" + audioCodec + 
+	"}:display\r\n")
     
     file.write("\r\n\r\n")
     file.write("## Control ## \r\n")
@@ -139,11 +167,20 @@ def createMosaicConfigFile(list):
     
     file.close
         
-
+def writeToLog(log):
+    today = date.today()
+    file = open(LogFilePath, "a")
+    file.write(str(today) + " : " + log + "\r\n")
+    file.close
+    
 def main():
-
     with open('mosaic-settings.json') as json_file:
         json_data = json.load(json_file)
+        writeToLogBoolean = json_data['app']['debug']
+        for item in json_data['mosaic']['ipcitem']:
+            if not item:
+                writeToLog("No protocols are present")
+                sys.exit()
         createMosaicConfigFile(json_data)
         createImageFile(json_data)
         createExecuteFile(json_data)
